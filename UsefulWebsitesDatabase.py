599c523c089c9965fa1e72b3867200842ad0e665
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-03-02 01:44:01 +0100

    Update UsefulWebsitesDatabase.py

--- UsefulWebsitesDatabase.py
+++ UsefulWebsitesDatabase.py
@@ -1,4 +1,7 @@
 {
+    'Gym':[
+        'https://musclewiki.com/' # gives you specific trainings for specified muscles
+    ],
     'BotDetection':[
         'https://iphey.com/', # digital identity trustworthiness
         'https://nowsecure.nl' # pass bot test, note: buggy website
@@ -6,6 +9,5 @@
     'General':[
         'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
         'https://playphrase.me', # returns scenes from famous movies containing the specified keyword/keyphrase
-        '', #
     ]
 }
