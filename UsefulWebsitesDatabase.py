402b60382e8177e114410c74aa5b2391d35b0aae
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-03-02 01:37:18 +0100

    Update UsefulWebsitesDatabase.py

--- UsefulWebsitesDatabase.py
+++ UsefulWebsitesDatabase.py
@@ -3,4 +3,9 @@
         'https://iphey.com/', # digital identity trustworthiness
         'https://nowsecure.nl' # pass bot test, note: buggy website
     ],
+    'General':[
+        'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
+        'https://playphrase.me', # returns scenes from famous movies containing the specified keyword/keyphrase
+        '', #
+    ]
 }
