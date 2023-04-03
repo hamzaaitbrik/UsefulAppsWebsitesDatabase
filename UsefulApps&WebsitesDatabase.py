00444e3039e760b17db3d4b5b15436ec5bd47f75
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-04-03 17:09:49 +0000

    Added Apps section

--- UsefulApps&WebsitesDatabase.py
+++ UsefulApps&WebsitesDatabase.py
@@ -1,13 +1,17 @@
 {
-    'Gym':[
-        'https://musclewiki.com/' # gives you specific trainings for specified muscles
+    'Websites':[
+            'Gym':[
+                'https://musclewiki.com/' # gives you specific trainings for specified muscles
+            ],
+            'BotDetection':[
+                'https://iphey.com/', # digital identity trustworthiness
+                'https://nowsecure.nl' # pass bot test, note: buggy website
+            ],
+            'General':[
+                'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
+                'https://playphrase.me', # returns scenes from famous movies containing the specified keyword/keyphrase
+            ]
     ],
-    'BotDetection':[
-        'https://iphey.com/', # digital identity trustworthiness
-        'https://nowsecure.nl' # pass bot test, note: buggy website
-    ],
-    'General':[
-        'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
-        'https://playphrase.me', # returns scenes from famous movies containing the specified keyword/keyphrase
+    'Apps':[
     ]
 }
