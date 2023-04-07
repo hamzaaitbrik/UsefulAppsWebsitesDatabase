56a75affdc2987eb21b09baffbac2a42eb73c703
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-04-07 14:52:34 +0000

    Added more resources

--- UsefulApps&WebsitesDatabase.py
+++ UsefulApps&WebsitesDatabase.py
@@ -8,7 +8,8 @@
                 'https://nowsecure.nl' # pass bot test, note: buggy website
             ],
             'Freemium':[
-                'https://12ft.io' # bypass any paywall
+                'https://12ft.io', # bypass any paywall
+                'https://tinywow.com/', # free alternatives to paid tools
             ],
             'General':[
                 'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
