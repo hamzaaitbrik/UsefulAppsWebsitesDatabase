f9aa58ff0a941000061b4d8cbbef45eb1e6afbf0
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-04-07 14:51:43 +0000

    Added more resources

--- UsefulApps&WebsitesDatabase.py
+++ UsefulApps&WebsitesDatabase.py
@@ -13,6 +13,7 @@
             'General':[
                 'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
                 'https://playphrase.me', # returns scenes from famous movies containing the specified keyword/keyphrase
+                'https://remove.bg/', # remove background from images
             ]
     ],
     'Apps':[
