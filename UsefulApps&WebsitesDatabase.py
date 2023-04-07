fbe328f2a7317e09a305142f8eae3ac1be23688d
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-04-07 14:50:20 +0000

    Added more resources

--- UsefulApps&WebsitesDatabase.py
+++ UsefulApps&WebsitesDatabase.py
@@ -7,6 +7,9 @@
                 'https://iphey.com/', # digital identity trustworthiness
                 'https://nowsecure.nl' # pass bot test, note: buggy website
             ],
+            'Freemium':[
+                'https://12ft.io' # bypass any paywall
+            ],
             'General':[
                 'https://web.extension.illinois.edu/stain/', # this website gives you the solutions to remove stains from clothes
                 'https://playphrase.me', # returns scenes from famous movies containing the specified keyword/keyphrase
